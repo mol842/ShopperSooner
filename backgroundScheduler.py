0a309e64223192f9e1a191389aff88566cb06d67
Molly <molly.stow@student.unimelb.edu.au>
2024-04-21 15:37:51 +1000

    first attempt at adding pickle for logs

--- backgroundScheduler.py
+++ backgroundScheduler.py
@@ -1,22 +1,32 @@
 import time
 from shopperNowScraper import scrapeNewJobs
 import config
+import pickle
+
 
 # added Northland just for testing, modify as needed
-DISPLAYUI = False
+DISPLAYUI = True
 MAXCYCLES = 100
 SLEEPTIME = 1000 # 3 mins idk
 
-KEYWORDS = ["Woolworths", "Dan", "Murphys", "Online", "Uber", "UberVCExpress", "Delivery", "Northland"]
+KEYWORDS = ["Woolworths", "Dan", "Murphys", "Online", "Uber", "UberVCExpress", "Delivery", "Optus", "BWS"]
+LOGFILE = "log.pk"
+
 
 ## import from config file
 #USERNAME = "***REMOVED***"
 #PASSWORD = "***REMOVED***"
 
+sentJobs = []
 
 i = 0
 while i < MAXCYCLES:
-   scrapeNewJobs(DISPLAYUI, KEYWORDS, config.USERNAME, config.PASSWORD)
+   sentJobs = scrapeNewJobs(DISPLAYUI, KEYWORDS, config.USERNAME, config.PASSWORD, sentJobs)
+
    i+=1
    print("Cycle: " + str(i))
+
+   with open(LOGFILE, 'wb') as fi:
+      pickle.dump(sentJobs, fi)
+
    time.sleep(SLEEPTIME)
